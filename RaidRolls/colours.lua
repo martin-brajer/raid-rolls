d4bc8d6661ffa362acf4dc9384d2849a61660e4e
Martin Brajer <martin.brajer@seznam.cz>
2022-04-10 09:50:57 +0200

    formatting and simplification

--- RaidRolls/colours.lua
+++ RaidRolls/colours.lua
@@ -1,32 +1,25 @@
+-- System message colour for unknown.
 local SystemMessageColour = "|cFFFFFF00"
 
 -- From https://wow.gamepedia.com/Class_colors.
--- System message colour for unknown.
 RaidRolls_G.classColours = {
-["HUNTER"] = "|cFFABD473",
-["WARLOCK"] = "|cFF8787ED",
-["PRIEST"] = "|cFFFFFFFF",
-["PALADIN"] = "|cFFF58CBA",
-["MAGE"] = "|cFF40C7EB",
-["ROGUE"] = "|cFFFFF569",
-["DRUID"] = "|cFFFF7D0A",
-["SHAMAN"] = "|cFF0070DE",
-["WARRIOR"] = "|cFFC79C6E",
-["DEATHKNIGHT"] = "|cFFC41F3B",
-["MONK"] = "|cFF00FF96",
-["DEMONHUNTER"] = "|cFFA330C9",
-["UNKNOWN"] = SystemMessageColour,
+    HUNTER = "|cFFABD473",
+    WARLOCK = "|cFF8787ED",
+    PRIEST = "|cFFFFFFFF",
+    PALADIN = "|cFFF58CBA",
+    MAGE = "|cFF40C7EB",
+    ROGUE = "|cFFFFF569",
+    DRUID = "|cFFFF7D0A",
+    SHAMAN = "|cFF0070DE",
+    WARRIOR = "|cFFC79C6E",
+    DEATHKNIGHT = "|cFFC41F3B",
+    MONK = "|cFF00FF96",
+    DEMONHUNTER = "|cFFA330C9",
+    UNKNOWN = SystemMessageColour,
 };
 
-
--- Group channel colours.
-local _channelColours = {
-["RAID"] = "|cFFFF7D00",
-["PARTY"] = "|cFFAAA7FF",
+RaidRolls_G.channelColours = {
+    RAID = "|cFFFF7D00",
+    PARTY = "|cFFAAA7FF",
+    UNKNOWN = SystemMessageColour,
 }
--- Function adds nil keyword.
-function RaidRolls_G.channelColours(channel)
-    -- System message colour (for unknown).
-    if (channel == nil) then return SystemMessageColour end
-    return _channelColours[channel]
-end
